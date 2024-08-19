015db0d137325a835007a93d1f656a3b3133678e
Nikki Mae Evers <37475750+nikkimae@users.noreply.github.com>
2024-08-19 18:23:03 +0200

    Initial commit - working solution

---
+++ Week-15/1-Setting-up-NodeJS/afternoon/solution/src/helpers.ts
@@ -0,0 +1,10 @@
+// export function getHelloMessage(): string {
+//     return "Hello from the helper module!";
+// }
+
+import { customMessage } from './config';
+
+export function getHelloMessage(): string {
+ return customMessage;
+};
+
---
+++ Week-15/1-Setting-up-NodeJS/afternoon/solution/src/index.ts
@@ -0,0 +1,9 @@
+import http from 'http';
+import { port } from './config';
+//import { getHelloMessage } from './helpers';  // Import the helper function
+import { logServerMessage } from './logger';
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Server is running!');
+}).listen(port, () => logServerMessage(`Server is running on port ${port}`));
