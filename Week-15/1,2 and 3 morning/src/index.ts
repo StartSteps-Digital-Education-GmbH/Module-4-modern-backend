470347d427182dc74ca234bf6809b7be408a02af
ahzia <ah.ziayosfi@gmail.com>
2024-08-21 13:06:01 +0200

    rename folder

---
+++ Week-15/1,2 and 3 morning/src/index.ts
@@ -0,0 +1,18 @@
+import http from "http"
+// let http = require("http"); //import
+import dotenv from "dotenv";
+import express, { Request, Response, NextFunction } from "express" //@types/express
+import router from './routes.js';
+
+dotenv.config();
+
+const port = process.env.PORT;
+const app = express();
+
+//middleware to pass data as json
+app.use(express.json());
+app.use(router);
+
+
+// create a server, express will create a Node server for us
+app.listen(port, () => console.log("server is running at: http://127.0.0.1:5001"))
