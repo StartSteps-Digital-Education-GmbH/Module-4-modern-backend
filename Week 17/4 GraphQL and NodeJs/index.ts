6ed3e5ec23b3423aa6dc09cba27acb8f62f403d6
ahzia <ah.ziayosfi@gmail.com>
2024-09-05 13:04:03 +0430

    setup apollo graphQL service with express

---
+++ Week 17/4 GraphQL and NodeJs/index.ts
@@ -0,0 +1,28 @@
+import { ApolloServer,gql } from "apollo-server-express";
+import express from "express";
+
+
+//Schema
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => "Hello, world!",
+    },
+};
+
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+await apolloServer.start();
+
+const app = express();
+
+apolloServer.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () => {
+    console.log(`Server ready at http://localhost:4000${apolloServer.graphqlPath}`);
+});
